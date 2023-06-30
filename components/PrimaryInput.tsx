0e2c9c975922ccbe1adf1b4230add89301ca93a2
Tom Padmanathan <padmanathantom@gmail.com>
2023-06-30 13:44:02 +0100

    Changed UI for checkout

--- components/PrimaryInput.tsx
+++ components/PrimaryInput.tsx
@@ -9,8 +9,10 @@ export default function PrimaryInput(props: any) {
                     ? 'block h-10 border border-black ' + props.addClass
                     : 'block h-10 border border-black'
             }
+            value={props.value}
             inputMode={props.inputMode}
             required={props.required}
+            onChange={props.onChange}
             onKeyPress={props.onKeyPress}
         />
     );
--- pages/checkout/new-checkout-session.tsx
+++ pages/checkout/new-checkout-session.tsx
@@ -13,12 +13,13 @@ const stripePromise: any = loadStripe(
 export default function App() {
     const [clientSecret, setClientSecret] = useState('');
     const { cart, setCart } = useContext(AppContext);
+    const [userData, setUserData] = useState<any>({ includeCutlery: false });
 
     async function fetchData() {
         const response = await fetch('/api/create-payment-intent', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ cart: cart }),
+            body: JSON.stringify({ cart: cart, userData: userData }),
         });
         const data = await response.json();
         setClientSecret(data.clientSecret);
@@ -36,6 +37,10 @@ export default function App() {
         appearance,
     };
 
+    useEffect(() => {
+        console.log(userData);
+    }, [userData]);
+
     return (
         <div className="mx-96 my-10 flex h-screen items-center justify-between">
             <section className="h-[45rem] w-[30rem] border-2 border-black p-10">
@@ -48,13 +53,24 @@ export default function App() {
                         required={true}
                         inputMode="numeric"
                         onKeyPress={(e: any) => {
-                            if (!/[0-9+]/.test(e.key)) {
-                                e.preventDefault();
-                            }
+                            if (!/[0-9]/.test(e.key)) e.preventDefault();
+                        }}
+                        onChange={(e: any) => {
+                            const newUserData = { ...userData };
+                            newUserData.phoneNumber = parseInt(e.target.value);
+                            setUserData(newUserData);
                         }}
                         addClass={removeArrowsFromInput}
                     />
-                    <PrimaryInput type="email" placeholder="Email (optional)" />
+                    <PrimaryInput
+                        type="email"
+                        placeholder="Email"
+                        onChange={(e: any) => {
+                            const newUserData = { ...userData };
+                            newUserData.email = e.target.value;
+                            setUserData(newUserData);
+                        }}
+                    />
                 </div>
                 <label htmlFor="address-line-1">Address</label>
                 <div className="mb-2 flex justify-between">
@@ -62,21 +78,61 @@ export default function App() {
                         placeholder="Address line 1"
                         id="address-line-1"
                         required={true}
+                        onChange={(e: any) => {
+                            const newUserData = { ...userData };
+                            newUserData.addressLine1 = e.target.value;
+                            setUserData(newUserData);
+                        }}
                     />
-                    <PrimaryInput placeholder="Address line 2 (optional)" />
+                    <PrimaryInput
+                        placeholder="Address line 2 (optional)"
+                        onChange={(e: any) => {
+                            const newUserData = { ...userData };
+                            newUserData.addressLine2 = e.target.value;
+                            setUserData(newUserData);
+                        }}
+                    />
                 </div>
                 <div className="mb-10 flex justify-between">
-                    <PrimaryInput placeholder="City" />
-                    <PrimaryInput placeholder="Postcode" />
+                    <PrimaryInput
+                        placeholder="City/Town"
+                        onChange={(e: any) => {
+                            const newUserData = { ...userData };
+                            newUserData.cityTown = e.target.value;
+                            setUserData(newUserData);
+                        }}
+                    />
+                    <PrimaryInput
+                        placeholder="Postcode"
+                        value={userData.postcode}
+                        onChange={(e: any) => {
+                            const newUserData = { ...userData };
+                            newUserData.postcode = e.target.value.toUpperCase();
+                            setUserData(newUserData);
+                        }}
+                    />
                 </div>
                 <label htmlFor="">Order Info</label>
                 <div className="flex justify-between">
                     <textarea
-                        placeholder="Leave us a note"
+                        placeholder="Leave us a note (optional)"
                         className="block h-10 resize-none border border-black"
+                        onChange={(e: any) => {
+                            const newUserData = { ...userData };
+                            newUserData.orderNote = e.target.value;
+                            setUserData(newUserData);
+                        }}
                     />
                     <label htmlFor="include-cutlery">Include Cutlery</label>
-                    <PrimaryInput type="checkbox" id="include-cutlery" />
+                    <PrimaryInput
+                        type="checkbox"
+                        id="include-cutlery"
+                        onChange={(e: any) => {
+                            const newUserData = { ...userData };
+                            newUserData.includeCutlery = e.target.checked;
+                            setUserData(newUserData);
+                        }}
+                    />
                 </div>
             </section>
 
