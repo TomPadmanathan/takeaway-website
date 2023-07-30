a65f37857d7c55c781d7bf7e249a0273026d9cd6
Tom Padmanathan <padmanathantom@gmail.com>
2023-07-30 15:01:19 +0100

    Removed unneccery comment

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -83,7 +83,6 @@ export default function Cart(props: props) {
                                                                   {capitaliseFirstChar(
                                                                       option
                                                                   )}{' '}
-                                                                  {/* Assuming option is a string */}
                                                               </li>
                                                           );
                                                       }
