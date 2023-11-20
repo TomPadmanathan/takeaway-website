605a7223ee58913605b438f1f10e569d8695c2c1
Tom Padmanathan <padmanathantom@gmail.com>
2023-11-20 00:29:07 +0000

    Added privacy Policy

--- components/page/Page.tsx
+++ components/page/Page.tsx
@@ -1,16 +1,24 @@
 interface props {
     title: string;
     children: JSX.Element;
+    subTitle: string;
 }
 
-export default function Page({ children, title }: props): JSX.Element {
+export default function Page({
+    children,
+    title,
+    subTitle,
+}: props): JSX.Element {
     return (
         <div className="bg-gradient-to-r from-pink to-lightpink">
-            <div className="flex h-64 items-center justify-center">
-                <h1 className="text-5xl text-white">{title}</h1>
+            <div className="flex h-64 items-center justify-center text-white">
+                <div className="block text-center">
+                    <h1 className="pb-2 text-5xl">{title}</h1>
+                    <h3>{subTitle}</h3>
+                </div>
             </div>
-            <section className="h-screen">
-                <div className="mx-10 flex h-screen justify-center rounded-t-xl bg-white">
+            <section>
+                <div className="mx-10 flex justify-center rounded-t-xl bg-white p-24">
                     <div className="block">{children}</div>
                 </div>
             </section>
