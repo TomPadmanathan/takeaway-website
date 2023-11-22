d35105ad6a717d4e837b93526dc67a0c27190cc4
Tom Padmanathan <padmanathantom@gmail.com>
2023-11-22 15:06:56 +0000

    Renamed cateringservice

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import TopNav from '@/components/page/nav/TopNav';
 import BottomNav from '@/components/page/nav/BottomNav';
 import Footer from '@/components/page/Footer';
 import Reviews from '@/components/homePage/Reviews';
-import CateringService from '@/components/homePage/Catering';
+import CateringService from '@/components/homePage/CateringService';
 import LandingSection from '@/components/homePage/LandingSection';
 
 export default function Home(): JSX.Element {
