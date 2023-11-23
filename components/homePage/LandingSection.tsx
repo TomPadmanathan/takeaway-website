b6b074bcdf2cd61535b9ebdc56ccac6d06426ed6
Tom Padmanathan <padmanathantom@gmail.com>
2023-11-23 01:44:54 +0000

    Moved btns from nav to landing page on sm size

--- components/homePage/LandingSection.tsx
+++ components/homePage/LandingSection.tsx
@@ -1,6 +1,7 @@
 // React/Next
 import Image from 'next/image';
 import { useRouter } from 'next/router';
+import { useState, useEffect } from 'react';
 
 // Assets
 import Logo from '@/assets/img/logo.png';
@@ -8,8 +9,28 @@ import Logo from '@/assets/img/logo.png';
 // Types/Interfaces
 import { NextRouter } from 'next/router';
 
+// Packages
+import jwt from 'jsonwebtoken';
+
 export default function LandingSection(): JSX.Element {
     const router: NextRouter = useRouter();
+
+    const [token, setToken] = useState<string | null>();
+    const [userId, setUserId] = useState<string>();
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        if (!token) return;
+        setToken(token);
+        interface decodedToken {
+            userId: string;
+            iat: number;
+            exp: number;
+        }
+        const decodedToken: any = jwt.decode(token);
+        const userId = decodedToken.userId;
+        setUserId(userId);
+    }, [token]);
 
     return (
         <section className="bg-image px-64 pb-0 2xl:px-52 xl:px-40 l:px-32 m:px-16 sm:px-0">
@@ -19,12 +40,40 @@ export default function LandingSection(): JSX.Element {
                     className="border-black w-72 border py-16 invert filter "
                     alt={'site-icon'}
                 />
-                <button
-                    className="h-14 rounded-lg border-[3px] border-white bg-blue px-10 text-lg font-bold text-white"
+                {/* <button
+                    className="hidden h-14 rounded-lg border-[3px] border-white bg-blue px-10 text-lg font-bold text-white sm:block"
                     onClick={(): Promise<boolean> => router.push('/order')}
                 >
                     Order Now
-                </button>
+                </button> */}
+                <div className="hidden sm:block">
+                    <button
+                        className="mx-5 h-10 w-52 rounded border-[3px] border-blue bg-white px-10 font-bold text-pink sm:mb-5 sm:px-5"
+                        onClick={(): Promise<boolean> => router.push('/order')}
+                    >
+                        Order Now
+                    </button>
+
+                    {token ? (
+                        <button
+                            className="mx-5 h-10 w-60 rounded border-[3px] border-blue bg-white px-10 font-bold text-pink m:px-5"
+                            onClick={(): Promise<boolean> =>
+                                router.push('/users/' + userId)
+                            }
+                        >
+                            Go to Account
+                        </button>
+                    ) : (
+                        <button
+                            className="mx-5 h-10 w-60 rounded border-[3px] border-blue bg-white px-10 font-bold text-pink m:px-5"
+                            onClick={(): Promise<boolean> =>
+                                router.push('/auth/login')
+                            }
+                        >
+                            Login or Register
+                        </button>
+                    )}
+                </div>
 
                 <section className="mt-10 rounded-xl rounded-b-none border-4 border-b-0 border-blue bg-pink px-20 l:px-10 sm:rounded-none sm:border-x-0 xs:px-6 2xs:px-4 ">
                     <h2 className="pb-4 pt-6 text-3xl text-white">
