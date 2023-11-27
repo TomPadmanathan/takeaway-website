79bf988495e724c7afeec357676ada33d252c967
Tom Padmanathan <padmanathantom@gmail.com>
2023-11-27 05:41:56 +0000

    Improved UI for order page

--- pages/order.tsx
+++ pages/order.tsx
@@ -1,28 +1,30 @@
 // React/Next
-import React, { useContext, useEffect, useState, useRef } from 'react';
+import React, { useContext, useState, ChangeEvent, useEffect } from 'react';
 import Image from 'next/image';
-import { NextRouter, useRouter } from 'next/router';
 
 // Components
-import Products from '@/components/Products';
-import Cart from '@/components/Cart';
+import BottomNav from '@/components/page/nav/BottomNav';
+import Footer from '@/components/page/Footer';
 
 // Types/Interfaces
-import { products } from '@/interfaces/products';
+import { products, product } from '@/interfaces/products';
+import { productNavButton } from '@/interfaces/productNav';
 import { config } from '@/interfaces/config';
-
-// Packages
-import jwt from 'jsonwebtoken';
+import { cart } from '@/interfaces/cart';
 
 // Context
 import { AppContext } from '@/context/AppContext';
+import { IconContext } from 'react-icons';
 
 // Utils
 import formatPrice from '@/utils/formatPrice';
 import CalculateCheckoutPrices from '@/utils/CalculateCheckoutPrices';
+import capitaliseFirstChar from '@/utils/capitaliseFirstChar';
+import capitaliseFirstCharWords from '@/utils/capitaliseFirstCharWords';
 
 // Assets
-import Black from '@/assets/img/black.png';
+import { HiSearch, HiX } from 'react-icons/hi';
+import tailwindConfig from '@/tailwind.config';
 
 interface props {
     productsData: products;
@@ -52,99 +54,292 @@ export async function getServerSideProps(): Promise<getServerSideProps> {
 }
 
 export default function Home({ productsData, configData }: props): JSX.Element {
+    const tailwindColors: any = tailwindConfig?.theme?.colors;
     const [search, setSearch] = useState<string>('');
     const [cartOpen, setCartOpen] = useState<boolean>(false);
+    const { cart } = useContext(AppContext);
+    const checkoutPrices = new CalculateCheckoutPrices(cart, configData);
+    const [activeProductNav, setActiveProductNav] =
+        useState<productNavButton>('popular');
+
+    const filteredData = searchProducts(search, productsData, activeProductNav);
+
+    const productNavButtons: string[] = [
+        'popular',
+        'chinese',
+        'japanese',
+        'korean',
+        'indonesian',
+        'thai',
+    ];
+
+    const inputContainer: string =
+        'my-4 flex items-center rounded-sm bg-lightergrey overflow-hidden';
+    const inputfield: string =
+        'h-14 bg-lightergrey pl-2 focus:outline-none w-80';
+
+    return (
+        <IconContext.Provider
+            value={{
+                color: tailwindColors.grey,
+                size: '22px',
+            }}
+        >
+            <div className="bg-white">
+                <BottomNav />
+                <div className="mx-[100px] h-[900px] px-[150px]">
+                    <div className="mb-32 flex h-40 items-center justify-between">
+                        <div className={inputContainer}>
+                            <HiSearch className="ml-4" />
+                            <input
+                                placeholder="Search"
+                                type="text"
+                                className={inputfield}
+                                value={search}
+                                onChange={(
+                                    event: React.ChangeEvent<HTMLInputElement>
+                                ): void => setSearch(event.target.value)}
+                            ></input>
+                        </div>
+                        {search ? null : (
+                            <div>
+                                {productNavButtons.map((button: any) => (
+                                    <button
+                                        onClick={(): void =>
+                                            setActiveProductNav(button)
+                                        }
+                                        key={button}
+                                        className={`mx-3 h-16 rounded-sm p-3 text-grey ${
+                                            activeProductNav == button
+                                                ? 'bg-lightgrey text-white'
+                                                : 'bg-lightergrey'
+                                        }`}
+                                    >
+                                        {capitaliseFirstChar(button)}
+                                    </button>
+                                ))}
+                            </div>
+                        )}
+                        <p className="flex w-40 items-center">
+                            {cart.length} {cart.length == 1 ? 'item' : 'items'}{' '}
+                            - £{formatPrice(checkoutPrices.subTotal)}
+                        </p>
+                    </div>
+
+                    <div className="flex justify-center">
+                        <div className="grid grid-cols-5 gap-5">
+                            {filteredData.map(
+                                (element: product): JSX.Element => (
+                                    <ProductTab
+                                        product={element}
+                                        key={element.product}
+                                    />
+                                )
+                            )}
+                        </div>
+                    </div>
+                </div>
+                <Footer />
+            </div>
+        </IconContext.Provider>
+    );
+}
+
+interface productTabProps {
+    product: product;
+}
+
+function ProductTab(props: productTabProps): JSX.Element {
+    const [open, setOpen] = useState<boolean>(false);
 
     return (
         <>
-            <Navbar
-                search={[search, setSearch]}
-                configData={configData}
-                cartOpen={[cartOpen, setCartOpen]}
-            />
-            <main>
-                <Products search={search} products={productsData} />
-            </main>
+            <div className="w-72 overflow-hidden rounded bg-lightergrey pb-2 text-center text-grey">
+                <Image
+                    src={props.product.image}
+                    className="aspect-[4/3] w-72 border border-black"
+                    alt={props.product.product + ' image'}
+                />
+                <h2 className="text-xl">
+                    {capitaliseFirstCharWords(props.product.product)}
+                </h2>
+                <p>£{formatPrice(props.product.price)}</p>
+                <div className="flex justify-center">
+                    <button
+                        onClick={() => setOpen(true)}
+                        className="h-10 w-32 rounded-sm bg-white"
+                    >
+                        Add to cart
+                    </button>
+                </div>
+            </div>
+
+            <AddCartModel product={props.product} open={[open, setOpen]} />
         </>
     );
 }
 
-interface navProps {
-    search: [string, React.Dispatch<React.SetStateAction<string>>];
-    cartOpen: [boolean, React.Dispatch<React.SetStateAction<boolean>>];
-    configData: config;
+interface AddCartModel {
+    open: [boolean, React.Dispatch<React.SetStateAction<boolean>>];
+    product: product;
 }
 
-function Navbar(props: navProps): JSX.Element {
-    const { cart } = useContext(AppContext);
-    const [cartOpen, setCartOpen] = props.cartOpen;
-    const [search, setSearch] = props.search;
-    const router: NextRouter = useRouter();
-    const checkoutPrices = new CalculateCheckoutPrices(cart, props.configData);
-    const [token, setToken] = useState<string | null>(null);
-    const userId = useRef<string>();
+function AddCartModel(props: AddCartModel): JSX.Element {
+    const { setCart } = useContext(AppContext);
+    const [open, setOpen] = props.open;
+    const [selectedOption, setSelectedOption] = useState<string[]>([]);
+    const [quantity, setQuantity] = useState<number>(1);
 
     useEffect((): void => {
-        setToken(localStorage.getItem('token'));
-        if (!token) return;
-        const decodedToken: jwt.JwtPayload | null | string = jwt.decode(token);
-        if (!decodedToken || typeof decodedToken != 'object') return;
-        userId.current = decodedToken.userId;
-    }, [token]);
+        if (!props.product.options) return;
+        const initialSelectedOption = props.product.options.map(
+            (subArray: string[]) => subArray[0]
+        );
+        setSelectedOption(initialSelectedOption);
+    }, [props.product.options]);
+
+    function handleAddItemCart(): void {
+        const newItem: products = [];
+
+        for (let index = quantity; index != 0; index--) {
+            newItem.push({
+                ...props.product,
+                options: selectedOption,
+            });
+        }
+        // Push items to cart
+        newItem.forEach((element: product) =>
+            setCart((prevCart: cart) => [...prevCart, element])
+        );
+        setOpen(false);
+        if (props.product.options) {
+            const resetSelectedOption = props.product.options.map(
+                (subArray: string[]) => subArray[0]
+            );
+            setSelectedOption(resetSelectedOption);
+        }
+        if (quantity != 1) setQuantity(1);
+    }
 
     return (
         <>
-            <nav className="mx-96 my-16 flex justify-between">
-                <Image
-                    src={Black}
-                    className="border-black h-20 w-40 border hover:cursor-pointer"
-                    alt={'site-icon'}
-                    onClick={(): void => {
-                        router.push('/');
-                        setSearch('');
-                        setCartOpen(false);
-                    }}
-                />
-
-                <div className="flex">
-                    <input
-                        placeholder="Search"
-                        type="text"
-                        className="border-black h-10 w-96 border"
-                        value={search}
-                        onChange={(
-                            event: React.ChangeEvent<HTMLInputElement>
-                        ): void => setSearch(event.target.value)}
-                    ></input>
-                </div>
-
-                <div className="flex">
+            <div
+                className={`fixed inset-0 items-center justify-center ${
+                    open ? 'flex' : 'hidden'
+                }`}
+            >
+                <div className="relative flex h-96 w-[40rem] items-center justify-center bg-white text-grey shadow-lg">
                     <button
-                        className="border-black h-10 w-32 overflow-hidden border"
-                        onClick={
-                            token
-                                ? (): Promise<boolean> =>
-                                      router.push(`/users/${userId.current}`)
-                                : (): Promise<boolean> =>
-                                      router.push('/auth/login')
-                        }
+                        onClick={(): void => setOpen(false)}
+                        className="absolute right-2 top-2"
                     >
-                        {token ? 'Go to account' : 'Login'}
+                        <HiX />
                     </button>
-                    <button
-                        onClick={(): void => setCartOpen(!cartOpen)}
-                        className="border-black h-10 w-10 overflow-hidden border"
-                    >
-                        Cart
-                    </button>
-                    <span>
-                        {cart.length} {cart.length == 1 ? 'item' : 'items'} - £
-                        {formatPrice(checkoutPrices.subTotal)}
-                    </span>
-                </div>
-            </nav>
+                    <div className="flex flex-col items-center">
+                        <h2 className="mb-20 text-2xl">
+                            {capitaliseFirstCharWords(props.product.product)}
+                        </h2>
 
-            <Cart isVisible={cartOpen} configData={props.configData} />
+                        <div className="mx-2 mb-20 flex items-center">
+                            <div
+                                className={`${
+                                    props.product.options ? 'mx-2' : ''
+                                }`}
+                            >
+                                <button
+                                    onClick={(): void => {
+                                        quantity > 1
+                                            ? setQuantity(quantity - 1)
+                                            : null;
+                                    }}
+                                >
+                                    -
+                                </button>
+                                <span className="mx-4">{quantity}</span>
+                                <button
+                                    onClick={(): void =>
+                                        setQuantity(quantity + 1)
+                                    }
+                                >
+                                    +
+                                </button>
+                            </div>
+
+                            {props.product.options &&
+                                props.product.options.map(
+                                    (subArray: any, index: number) => (
+                                        <div className="mx-2 bg-lightergrey pr-3">
+                                            <select
+                                                key={subArray}
+                                                onChange={(
+                                                    event: ChangeEvent<HTMLSelectElement>
+                                                ) => {
+                                                    const updatedOptions =
+                                                        selectedOption
+                                                            ? [
+                                                                  ...selectedOption,
+                                                              ]
+                                                            : [];
+                                                    updatedOptions[index] =
+                                                        event.target.value;
+                                                    setSelectedOption(
+                                                        updatedOptions
+                                                    );
+                                                }}
+                                                value={
+                                                    selectedOption
+                                                        ? selectedOption[index]
+                                                        : ''
+                                                }
+                                                className="bg-lightergrey py-2 pl-3"
+                                            >
+                                                {subArray.map(
+                                                    (element: string) => (
+                                                        <option key={element}>
+                                                            {capitaliseFirstChar(
+                                                                element
+                                                            )}
+                                                        </option>
+                                                    )
+                                                )}
+                                            </select>
+                                        </div>
+                                    )
+                                )}
+                        </div>
+
+                        <button
+                            onClick={(): void => handleAddItemCart()}
+                            className="h-10 w-32 rounded-sm bg-lightergrey"
+                        >
+                            Add to cart
+                        </button>
+                    </div>
+                </div>
+            </div>
         </>
     );
+}
+
+function searchProducts(
+    query: string,
+    products: products,
+    activeProductNav: productNavButton
+): products {
+    return query
+        ? products.filter((element: product): boolean => {
+              const productName: string = element.product.toLowerCase();
+              const category: string = element.category
+                  .map((currentValue: string): string =>
+                      currentValue.toLowerCase()
+                  )
+                  .join(' ');
+              return (
+                  productName.includes(query.toLowerCase()) ||
+                  category.includes(query.toLowerCase())
+              );
+          })
+        : products.filter((element: product): boolean =>
+              element.category.includes(activeProductNav)
+          );
 }
