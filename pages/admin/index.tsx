bb18187641bc13a42e30f6b293c9d9b9231baedd
Tom Padmanathan <padmanathantom@gmail.com>
2023-12-02 08:48:27 +0000

    updated admin nav

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -2,5 +2,9 @@
 import AdminNav from '@/components/adminDashboard/AdminNav';
 
 export default function adminDashboard(): JSX.Element {
-    return <AdminNav />;
+    return (
+        <div className="h-screen">
+            <AdminNav />
+        </div>
+    );
 }
