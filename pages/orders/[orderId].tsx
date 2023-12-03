995b58c26b853b6eb1e93c7bae551d6241b88c1e
Tom Padmanathan <padmanathantom@gmail.com>
2023-12-03 04:25:45 +0000

    Added review ui for orders

--- pages/orders/[orderId].tsx
+++ pages/orders/[orderId].tsx
@@ -1,6 +1,6 @@
 // React/Next
 import { useState, useEffect } from 'react';
-import { NextPageContext } from 'next';
+import { useRouter } from 'next/router';
 
 // Database Models
 import Order from '@/database/models/Order';
@@ -13,11 +13,16 @@ import fetchWithToken from '@/utils/JWT/fetchWithToken';
 
 // Components
 import ListItemsWithPrice from '@/components/ListItemsWithPrice';
+import BottomNav from '@/components/page/nav/BottomNav';
+import Footer from '@/components/page/Footer';
 
 // Types/Interfaces
 import { config } from '@/interfaces/config';
-import { NextRouter, useRouter } from 'next/router';
-import SecondaryButton from '@/components/SecondaryButton';
+import { NextRouter } from 'next/router';
+
+// Assets
+import { HiStar } from 'react-icons/hi';
+import tailwindConfig from '@/tailwind.config';
 
 interface props {
     configData: config;
@@ -44,6 +49,11 @@ export default function OrderId(props: props): JSX.Element {
     const [products, setProducts] = useState([]);
     const router: NextRouter = useRouter();
     const [token, setToken] = useState<string | null>();
+    const [rating, setRating] = useState<number | null>(null);
+    const [starHover, setStarHover] = useState<number | null>(null);
+    const tailwindColors: any = tailwindConfig.theme?.colors;
+
+    useEffect((): void => console.log(rating), [rating]);
 
     useEffect((): void => {
         const token = localStorage.getItem('token');
@@ -75,17 +85,18 @@ export default function OrderId(props: props): JSX.Element {
             setProducts(result);
         }
         fetchOrder();
-    });
+    }, []);
     if (token)
         if (order)
             return (
                 <>
-                    <center className="mt-10 pb-10">
+                    <BottomNav />
+                    <section className="my-10 text-center">
                         <h2 className="text-4xl">Your order is:</h2>
                         <h1 className="text-[80px]">
                             {order.status.toUpperCase()}
                         </h1>
-                        {order.status === 'delivered' ? null : (
+                        {order.status !== 'delivered' && (
                             <>
                                 <h2>Estimated Delivery Time:</h2>
                                 <h2>
@@ -98,84 +109,151 @@ export default function OrderId(props: props): JSX.Element {
                                 </h2>
                             </>
                         )}
-                    </center>
-                    <div className="mx-96 flex justify-between">
-                        <section className="h-[35rem] w-[30rem] border-2 border-black p-10">
+                    </section>
+                    <div
+                        className={`flex w-screen items-center justify-center ${
+                            order.status !== 'delivered' && 'hidden'
+                        }`}
+                    >
+                        <section className=" w-1/3 rounded bg-white p-10">
+                            <div className="grid place-items-center">
+                                <h2 className=" text-xl text-grey2">
+                                    Hi {order.user.forename}, how was your meal?
+                                </h2>
+                                <form className="my-5 flex w-96 justify-center">
+                                    {new Array(5)
+                                        .fill(0)
+                                        .map((star: unknown, index: number) => {
+                                            const currentRating: number =
+                                                index + 1;
+
+                                            return (
+                                                <label>
+                                                    <input
+                                                        type="radio"
+                                                        name="rating"
+                                                        value={currentRating}
+                                                        onClick={(): void =>
+                                                            setRating(
+                                                                currentRating
+                                                            )
+                                                        }
+                                                        className="hidden"
+                                                    />
+                                                    <HiStar
+                                                        color={
+                                                            currentRating <=
+                                                            ((starHover as number) ||
+                                                                (rating as number))
+                                                                ? tailwindColors.yellow
+                                                                : tailwindColors.lightergrey
+                                                        }
+                                                        className="cursor-pointer"
+                                                        size={32}
+                                                        onMouseEnter={(): void =>
+                                                            setStarHover(
+                                                                currentRating
+                                                            )
+                                                        }
+                                                        onMouseLeave={(): void =>
+                                                            setStarHover(null)
+                                                        }
+                                                    />
+                                                </label>
+                                            );
+                                        })}
+                                </form>
+                                <textarea
+                                    className="mb-5 h-32 w-96 resize-none rounded-sm bg-lightergrey p-2 focus:outline-none"
+                                    placeholder="Tell us more about your experience"
+                                />
+                                <button
+                                    type="submit"
+                                    className="mr-2 h-16 rounded-sm bg-lightergrey px-3 text-grey transition-all hover:bg-lightgrey hover:text-white 3xs:h-12 3xs:px-2"
+                                >
+                                    Leave Review
+                                </button>
+                            </div>
+                        </section>
+                    </div>
+
+                    <div className="mb-10 flex justify-around">
+                        <section className="my-5 h-[720px] w-[480px] rounded bg-white p-5 shadow-lg">
                             <ul className="text-2xl leading-10">
                                 <li>
-                                    <span className="text-slate-500">
+                                    <p>
                                         Date:{' '}
-                                    </span>
-                                    {getDateFromTimestamp(
-                                        parseInt(order.timestamp)
-                                    )}
+                                        {getDateFromTimestamp(
+                                            parseInt(order.timestamp)
+                                        )}
+                                    </p>
                                 </li>
                                 <li>
-                                    <span className="text-slate-500">
-                                        Time:{' '}
-                                    </span>
-                                    {getTimeFromTimestamp(
-                                        parseInt(order.timestamp)
-                                    )}
+                                    <p>
+                                        Time:
+                                        {getTimeFromTimestamp(
+                                            parseInt(order.timestamp)
+                                        )}
+                                    </p>
                                 </li>
                                 <li>
-                                    <span className="text-slate-500">
-                                        Email:{' '}
-                                    </span>
-                                    {order.user.email}
+                                    <p>
+                                        Email:
+                                        {order.user.email}
+                                    </p>
                                 </li>
                                 <li>
-                                    <span className="text-slate-500">
-                                        Name:{' '}
-                                    </span>
-                                    {order.user.forename +
-                                        ' ' +
-                                        order.user.surname}
+                                    <p>
+                                        Name:
+                                        {order.user.forename +
+                                            ' ' +
+                                            order.user.surname}
+                                    </p>
                                 </li>
                                 <li>
-                                    <span className="text-slate-500">
-                                        Phone Number:{' '}
-                                    </span>
-                                    {order.user.phoneNumber}
+                                    <p>
+                                        Phone Number:
+                                        {order.user.phoneNumber}
+                                    </p>
                                 </li>
                                 <li>
-                                    <span className="text-slate-500">
-                                        City/Town:{' '}
-                                    </span>
-                                    {order.user.cityTown}
+                                    <p>
+                                        City/Town:
+                                        {order.user.cityTown}
+                                    </p>
                                 </li>
                                 <li>
-                                    <span className="text-slate-500">
-                                        Address Line 1:{' '}
-                                    </span>
-                                    {order.user.addressLine1}
+                                    <p>
+                                        Address Line 1:
+                                        {order.user.addressLine1}
+                                    </p>
                                 </li>
                                 {order.user.addressLine2 ? (
                                     <li>
-                                        <span className="text-slate-500">
-                                            Address Line 2:{' '}
-                                        </span>
-                                        {order.user.addressLine2}
+                                        <p>
+                                            Address Line 2:
+                                            {order.user.addressLine2}
+                                        </p>
                                     </li>
                                 ) : null}
                                 <li>
-                                    <span className="text-slate-500">
-                                        Postcode:{' '}
-                                    </span>
-                                    {order.user.postcode}
+                                    <p>
+                                        Postcode:
+                                        {order.user.postcode}
+                                    </p>
                                 </li>
                                 <li>
-                                    <span className="text-slate-500">
-                                        Order Id:{' '}
-                                    </span>
-                                    {order.orderId}
+                                    <p>
+                                        Order Id:
+                                        {order.orderId}
+                                    </p>
                                 </li>
                                 {order.orderNote ? (
                                     <li>
-                                        <span className="text-slate-500">
+                                        <p>
                                             Order Note:
-                                        </span>
-                                        {order.orderNote}
+                                            {order.orderNote}
+                                        </p>
                                     </li>
                                 ) : null}
                             </ul>
@@ -185,6 +263,7 @@ export default function OrderId(props: props): JSX.Element {
                             config={props.configData}
                         />
                     </div>
+                    <Footer />
                 </>
             );
         else
